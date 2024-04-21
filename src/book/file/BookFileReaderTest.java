ac5d366af2605241f4b1f8689523bb3b778fedc7
mac <muhammedowoyale@gmail.com>
2024-04-21 23:41:08 +0100

    First commit

--- src/book/file/BookFileReaderTest.java
+++ src/book/file/BookFileReaderTest.java
@@ -77,6 +77,7 @@ class BookFileReaderTest {
 				"BOOK ONE: 1805",
 				"CHAPTER I",
 				"CHAPTER II"
+
 		};
 		List<String> expectedNext10WarAndPeace = new ArrayList<String>(Arrays.asList(expectedNext10WarAndPeaceArray));
 
